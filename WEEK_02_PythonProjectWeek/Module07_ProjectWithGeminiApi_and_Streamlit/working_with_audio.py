47f8cb34432dcab1f9aaaaa7e83371bbe3517672
AmmarHamim <ammarhamim7@gmail.com>
2026-04-18 20:07:35 +0600

    w2ProjectDone

---
+++ WEEK_02_PythonProjectWeek/Module07_ProjectWithGeminiApi_and_Streamlit/working_with_audio.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+import streamlit as st
+import io
+
+text="Hello i am hamim and i am learning ai/ml"
+
+speech=gTTS(text,lang='en',slow=False)
+
+# speech.save("welcome.mp3") 
+
+audio_buffer=io.BytesIO()
+speech.write_to_fp(audio_buffer)
+st.audio(audio_buffer)
